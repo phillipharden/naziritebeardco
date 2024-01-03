3d19b771e62eeedaa6d4c438c878e662cd06de04
PhillipHarden <phillipiscoding@gmail.com>
2024-01-02 19:24:58 -0500

    section 3 complete

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import {
   Row,
@@ -14,12 +14,22 @@ import {
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import products from '../products';
+// import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = () => {
+  const [ product, setProduct ] = useState({});
+
   const { id: productId } = useParams();
 
-  const product = products.find((p) => p._id === productId);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
