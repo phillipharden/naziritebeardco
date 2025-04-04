0b2ff36c2f59afac64cedd9f16cef2a38a9fabde
PhillipHarden <phillipiscoding@gmail.com>
2025-04-04 15:17:42 -0400

    runs

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import products from './data/products.js';
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js';
+
 const port = process.env.PORT || 4444;
 
-connectDB(); //Connect to MongoDB
+// connectDB(); //Connect to MongoDB
 
 const app = express();
 
@@ -14,9 +13,14 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.use('/api/products', productRoutes);
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
 
-app.use(notFound); 
-app.use(errorHandler);
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
